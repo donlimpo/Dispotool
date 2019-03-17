0160f5dee6201d6ce98dc5ee567d4c11b7ed611e
Heiko <Heiko@heikos-mac-book.fritz.box>
2019-03-17 19:18:28 +0100

    Zur Übernahme

--- Dispotool_1/src/DispoMain.java
+++ Dispotool_1/src/DispoMain.java
@@ -10,6 +10,7 @@ public class DispoMain {
 		// TODO Auto-generated method stub
 		System.out.println("Houston...");
 		ArrayList<String[]> Basisdaten =  new ArrayList<String[]>(); 
+		ArrayList<String> Inhalt = new ArrayList<String>();
 		
 		/* hier beginnt das Laden*/
 		 String csvFile = "BCG_Basis_Netz_20190315-1.csv";
@@ -23,9 +24,10 @@ public class DispoMain {
 
 	                // use comma as separator
 	                String[] Kante = line.split(cvsSplitBy);
-
+	                Inhalt.add(line);
 	                System.out.println("Kante [von= " + Kante[0] + " => Nach=" + Kante[1] + "] Dauer: "+ Kante[4]);
-	                System.out.println(Kante.toString());
+	                System.out.println(Kante[1].toString());
+	                Arrays.deepToString(Basisdaten);
 	                Basisdaten.add(Kante);
 
 	            }
@@ -33,6 +35,9 @@ public class DispoMain {
 	        } catch (IOException e) {
 	            e.printStackTrace();
 	        }
+	        System.out.println("Hier geben wir eine Liste aus");
+	        
+	        System.out.println(Arrays.toString(Basisdaten[1]));
 	       
 	}
 }
