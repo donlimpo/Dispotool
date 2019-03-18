d2ed2d1fb2998381ccea19f5c36a711fd6f63585
donlimpo <heiko@guethenke.de>
2019-03-18 06:31:11 +0100

    Erster Lambda Ausdruck

--- Dispotool_1/src/DispoMain.java
+++ Dispotool_1/src/DispoMain.java
@@ -3,33 +3,45 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class DispoMain {
+	private ArrayList<Kanten> ALKanten;
+
+/*
+ * Quelle: https://examples.javacodegeeks.com/core-java/java-8-stream-collectors-groupingby-example/
+ */
+public	Map<String, List<Kanten>> groupByKnoten() {
+	
+
+	return ALKanten.stream().collect(Collectors.groupingBy(Kanten::getVon));
+
+}
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Houston...");
 
-		ArrayList<String> Inhalt = new ArrayList<String>();
 		ArrayList<Kanten> Vertex = new ArrayList<Kanten>(); 
 		
 		/* hier beginnt das Laden*/
-		 String csvFile = "BCG_Basis_Netz_20190315-1.csv";
-		
-	        String line = "";
-	        String cvsSplitBy = ";";
+		String csvFile = "BCG_Basis_Netz_20190315-1.csv";
+		int iZaehler = 0;
+        String line = "";
+        String cvsSplitBy = ";";
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
 
-	        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+            while ((line = br.readLine()) != null) {
 
-	            while ((line = br.readLine()) != null) {
+                // use comma as separator
+                String[] Kante = line.split(cvsSplitBy);
+                System.out.println("Kante [von= " + Kante[0] + " => Nach=" + Kante[1] + "] Dauer: "+ Kante[4]);
 
-	                // use comma as separator
-	                String[] Kante = line.split(cvsSplitBy);
-	                System.out.println("Kante [von= " + Kante[0] + " => Nach=" + Kante[1] + "] Dauer: "+ Kante[4]);
-	                System.out.println(Kante[1].toString());
-	                Kanten TempKante = new Kanten();
-	                TempKante.Von =Kante[0];
-	                TempKante.Nach = Kante[1];
+                Kanten TempKante = new Kanten();
+                TempKante.Von =Kante[0];
+                TempKante.Nach = Kante[1];
+                if (iZaehler >0) { /* sorgt dafür, das die erste Zeile nicht gelesen wird*/
 	                try {
 	                	TempKante.Dauer = Integer.parseInt(Kante[4]);
 	                }
@@ -40,14 +52,16 @@ public class DispoMain {
 	               /* Achtung: die erste Zeile enthält einen Namenud kann daher einen Fehler werfen.
 	                */
 	                Vertex.add(TempKante);
-	            }
+                }
+                iZaehler++;
+	           }
 
 	        } catch (IOException e) {
 	            e.printStackTrace();
 	        }
-	        System.out.println("Hier geben wir eine Liste aus");
+	        System.out.println("Es wurden "+  iZaehler + " Zeilen geladen");
 	        
-	        System.out.println(Vertex.get(0).Von);
+	       // System.out.println(Vertex.get(0).Von);
 	       
 	}
 }
--- Dispotool_1/src/Kanten.java
+++ Dispotool_1/src/Kanten.java
@@ -3,5 +3,15 @@ public class Kanten {
 	String Von;
 	String Nach;
 	int Dauer;
+	
+	public String getVon() {
+		return this.Von;
+	}
+	public String getNach() {
+		return this.Nach;
+	}
+	public int getDauer() {
+		return this.Dauer;
+	}
 
 }
